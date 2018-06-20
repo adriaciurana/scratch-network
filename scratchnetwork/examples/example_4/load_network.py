b0e13606f2c10de4fb5e1fa81037654b6b90e926
Adrià <adri00796@gmail.com>
2018-06-20 15:38:34 +0200

    no message

--- scratchnetwork/examples/example_4/load_network.py
+++ scratchnetwork/examples/example_4/load_network.py
@@ -23,6 +23,20 @@ print(images_test.shape)
 
 net = Network()
 # DESCARGAR LOS PESOS EN: https://drive.google.com/file/d/1DeewIZLeeEUqVIsWrcPwc1Y3zr7eFUDA/view?usp=sharing
+"""
+accuracy: 0.9572
+Confusion Matrix:
+[[ 972    0    1    0    0    1    4    1    1    0]
+ [   0 1115    7    3    1    0    8    0    1    0]
+ [  10    1  998   11    3    0    2    6    1    0]
+ [   1    0    8  977    0   13    0    9    1    1]
+ [   2    0    1    0  954    0    8    0    1   16]
+ [   5    0    0    9    0  868    9    0    0    1]
+ [  11    1    0    0    4    2  940    0    0    0]
+ [   2    3   25    4    3    0    0  981    1    9]
+ [  36    2   24   14    3   24   15   11  825   20]
+ [   8    6    2   17   12   12    0   10    0  942]]
+"""
 net.load('example.h5')
 
 out = net.predict({'Input': images_test})
